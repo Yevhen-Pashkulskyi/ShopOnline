034ccd694300ff7faceaddd45173a38ab34db6cb
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-15 23:55:46 +0200

    added a method to refine the purchase

--- src/orders/InStore.java
+++ src/orders/InStore.java
@@ -2,11 +2,27 @@ package orders;
 
 import model.Order;
 
+import java.util.Scanner;
+
 public class InStore implements Order {
     private static final String CURRENCY = "USD";
     private String productName;
     private int quantity;
-    private  int price;
+    private int price;
+
+    private void enterOrderDetails() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть назву товару: ");
+        productName = scanner.nextLine();
+
+        System.out.printf("Введіть кількість %s: ", productName);
+        quantity = scanner.nextInt();
+
+        System.out.printf("Введіть вартість %s в %s за шт.:", productName, CURRENCY);
+        price = scanner.nextInt();
+    }
+
     @Override
     public double calclulateTotalCost() {
         return 0;
