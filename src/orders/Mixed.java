be025a1a40d3a464d8696b1c42869377661bae32
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-16 00:52:04 +0200

    added a method to refine the purchase to Mixed

--- src/orders/Mixed.java
+++ src/orders/Mixed.java
@@ -2,6 +2,8 @@ package orders;
 
 import model.Order;
 
+import java.util.Scanner;
+
 public class Mixed implements Order {
     private static final double DELIVERY_COST_PERCENTAGE = 20.0;
     private static final String CURRENCY = "USD";
@@ -10,6 +12,25 @@ public class Mixed implements Order {
     private int deliveryQuantity;
     private double price;
 
+    public Mixed() {
+        enterOrderDetails();
+    }
+
+    private void enterOrderDetails() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введіть назву товару: ");
+        productName = scanner.nextLine();
+
+        System.out.printf("Введіть кількість %s для самовивозу: ", productName);
+        pickupQuantity = scanner.nextInt();
+
+        System.out.printf("Введіть кількість %s для доставки: ", productName);
+        deliveryQuantity = scanner.nextInt();
+
+        System.out.printf("Введіть вартість за позицію в %s: ", CURRENCY);
+        price = scanner.nextDouble();
+    }
     @Override
     public double calculateTotalCost() {
         return 0;
