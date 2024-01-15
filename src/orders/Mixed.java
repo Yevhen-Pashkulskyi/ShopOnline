5997b60ba5030ccd683acfc6e18f1a120e7b8de1
Pashkulskyi Yevhen <pashkulskyevhen@gmail.com>
2024-01-16 00:48:10 +0200

    add fields to Mixed

--- src/orders/Mixed.java
+++ src/orders/Mixed.java
@@ -1,4 +1,17 @@
 package orders;
 
-public class Mixed {
+import model.Order;
+
+public class Mixed implements Order {
+    private static final double DELIVERY_COST_PERCENTAGE = 20.0;
+    private static final String CURRENCY = "USD";
+    private String productName;
+    private int pickupQuantity;
+    private int deliveryQuantity;
+    private double price;
+
+    @Override
+    public double calculateTotalCost() {
+        return 0;
+    }
 }
